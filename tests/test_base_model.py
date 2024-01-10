6fff18ecaa9744b722162cbec5148450969d8f2c
MACRODAT <macrodatservices@gmail.com>
2024-01-10 23:19:29 +0100

    Changes to the file

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -7,9 +7,9 @@ Unit testing the test_base
 import sys
 from os.path import join, dirname
 sys.path.append(join(dirname(__file__), ".."))
-from models.base_model import *
-import unittest
-import datetime as dt
+from models.base_model import *  # noqa: E402
+import unittest  # noqa: E402
+import datetime as dt  # noqa: E402
 
 
 class TestBase(unittest.TestCase):
@@ -19,21 +19,24 @@ class TestBase(unittest.TestCase):
 
     def test_init(self):
         b = BaseModel()
-        self.assertEqual(type(b), BaseModel, 
+        self.assertEqual(type(b), BaseModel,
                          "Type mismatch")
 
     def test_id(self):
-        l = []
+        list_ = []
         for _ in range(100):
             b = BaseModel()
-            self.assertFalse(b.id in l, "UUID exists")
-            l.append(b.id)
+            self.assertFalse(b.id in list_, "UUID exists")
+            list_.append(b.id)
 
     def test_dates(self):
         o = dt.datetime.now()
         b = BaseModel()
         self.assertIsNotNone(b.created_at,
-                               "Timing problem")
-    
+                             "Timing problem")
+        self.assertIsNotNone(b.updated_at,
+                             "Timing problem")
+
+
 if __name__ == "__main__":
     unittest.main()
