b2157228c69ebd9f7fc1e055980fcd36d512aef1
MACRODAT <macrodatservices@gmail.com>
2024-01-10 22:32:26 +0100

    Changes to the file

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -4,14 +4,33 @@
 Unit testing the test_base
 """
 
-
+from models.base_model import *
 import unittest
+import datetime as dt
 
 
-class TestBase:
+class TestBase(unittest.TestCase):
     '''
     class to test the test base model
     '''
 
     def test_init(self):
-        
+        b = BaseModel()
+        self.assertEqual(type(b), BaseModel, 
+                         "Type mismatch")
+
+    def test_id(self):
+        l = []
+        for _ in range(100):
+            b = BaseModel()
+            self.assertFalse(b.id in l, "UUID exists")
+            l.append(b.id)
+
+    def test_dates(self):
+        o = dt.datetime.now()
+        b = BaseModel()
+        self.assertAlmostEqual(b.created_at, o, 
+                               None, "Timing problem")
+        self.assertAlmostEqual(b.updated_at, o, 
+                               None, "Timing problem")
+    
