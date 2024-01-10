7d20ed1c89c80ada62bc921d92a8d76bc82792b7
MACRODAT <macrodatservices@gmail.com>
2024-01-10 21:49:28 +0100

    Changes to the file

---
+++ tests/test_base_model.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Unit testing the test_base
+"""
+
+
+import unittest
+
+
+class TestBase:
+    '''
+    class to test the test base model
+    '''
+
+    def test_init(self):
+        
