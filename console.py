848e92f869db40896854c263a4f466bfb8f9c392
MACRODAT <macrodatservices@gmail.com>
2024-01-11 22:24:11 +0100

    Changes to the file

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ Main command line interface
 
 
 import cmd
+from typing import Any
 
 
 class HBNBCommand(cmd.Cmd):
@@ -16,8 +17,19 @@ class HBNBCommand(cmd.Cmd):
     intro = "Welcome to HBNB-MACRODAT command prompt."
     prompt = "(hbnb)"
 
+    def cmdloop(self, intro: Any | None = None) -> None:
+        """loop function"""
+        return super().cmdloop(intro)
 
-    pass
+    def do_quit(self, obj):
+        """quit"""
+        return self.do_EOF(obj)
+
+
+    def do_EOF(self, obj):
+        """quit"""
+        print("Exiting.")
+        return True
 
 
 if __name__ == '__main__':
