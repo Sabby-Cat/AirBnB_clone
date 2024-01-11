345f27b49a35e1d80e2b4105c2c9965f5f0a0f54
MACRODAT <macrodatservices@gmail.com>
2024-01-11 21:41:14 +0100

    Changes to the file

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Main command line interface
+"""
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    class to take care of command stuff
+    """
+
+    pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
